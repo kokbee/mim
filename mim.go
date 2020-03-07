1fa4f44d028af51e1540c1aaf4013f97c6f83d66
김민수 <kokbee@kokbee.local>
2020-03-07 23:46:59 +0900

    add code

--- mim.go
+++ mim.go
@@ -3,11 +3,33 @@ package main
 import (
 	"flag"
 	"fmt"
-	"html/template"
+	_"html/template"
 	"log"
-	"os"
+	_"os"
+)
+
+var (
+	flagHttpPort = flag.String("http", "", "Web Port Number")
 )
 
 func main() {
-	log 
+	log.SetFlags( log.LstdFlags | log.Lshortfile)
+	log.SetPrefix("mim : ")
+	flag.Parse()
+
+	ip, err := serverData()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if  *flagHttpPort != "" {
+		if *flagHttpPort == ":80" {
+			fmt.Printf("Http://%s",ip)
+
+		}else if *flagHttpPort == ":443" {
+			fmt.Printf("Https://%s",ip)
+		}else{
+			fmt.Printf("Dev Mode")
+			fmt.Printf("Http://%s%s\n", ip, flagHttpPort)
+		}
+	}
 }
