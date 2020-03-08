d476c602d18d0288da079b3061160fed280757bc
김민수 <kokbee@kokbee.local>
2020-03-08 20:51:25 +0900

    modify

--- web.go
+++ web.go
@@ -1,21 +1,58 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content Type", "text/html")
-	http.ServeFile(w, r, "assets/template/base.html")
+// 메인
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	index := template.Must(template.ParseFiles(
+		"assets/template/header.html",
+		"assets/template/base.html",
+		"assets/template/footer.html",
+	))
+	err := index.Execute(w, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// 파일에 따른 타입 값
+func staticType(path string) string {
+	var contentType string
+	ext := filepath.Ext(path)
+	switch ext {
+	case ".html":
+		contentType = "text/html"
+	case ".css":
+		contentType = "text/css"
+	case ".js":
+		contentType = "application/javascript"
+	case ".jpg":
+		contentType = "image/jpeg"
+	case ".png":
+		contentType = "image/png"
+	default:
+		contentType = "text/plain"
+	}
+
+	return contentType
 }
 
 func webPage(port string) {
-	// '/assets/' 경로에 접근시 파일서버 동작, 경로삭제후 'assets'으로 만 표시
-	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	// assets 폴더
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("assets/css"))))
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("assets/js"))))
+	http.Handle("/template/", http.StripPrefix("/template/", http.FileServer(http.Dir("assets/template"))))
+	http.Handle("/bootstrap/", http.StripPrefix("/bootstrap/", http.FileServer(http.Dir("assets/bootstrap_3.3.2"))))
+	// vendor 폴더
+	http.Handle("/vendor/", http.StripPrefix("/vendor/", http.FileServer(http.Dir("assets/vendor"))))
 
 	// main
-	http.HandleFunc("/", Index)
+	http.HandleFunc("/", indexHandler)
 
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
